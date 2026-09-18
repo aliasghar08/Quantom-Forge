cf72e69cbfff0a2ec3b0146cc4020251e12ca449
Ali Asghar <aliasgharinnocent@yahoo.com>
2026-09-18 13:27:59 +0500

    Fix ReactionLibraryScreen grid overflow by using mainAxisExtent

--- quantum_forge/lib/features/reaction_library/presentation/screens/library_screen.dart
+++ quantum_forge/lib/features/reaction_library/presentation/screens/library_screen.dart
@@ -211,7 +211,7 @@ class _LibraryScreenState extends State<LibraryScreen> {
           crossAxisCount: 3,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
-          childAspectRatio: 0.88,
+          mainAxisExtent: 320, // Generous fixed height prevents overflow
         ),
         itemCount: items.length,
         itemBuilder: (_, i) => ReactionCardWidget(
