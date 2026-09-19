1396d3d0a7e3d7c2143b843381502df1b8d420d9
Ali Asghar <aliasgharinnocent@yahoo.com>
2026-09-19 12:23:42 +0500

    fix: import scheduler for Ticker

--- quantum_forge/lib/features/reaction_runner/presentation/widgets/interactive_builder_widget.dart
+++ quantum_forge/lib/features/reaction_runner/presentation/widgets/interactive_builder_widget.dart
@@ -1,5 +1,6 @@
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:quantum_forge/core/utils/xyz_parser.dart';
 
 enum BuilderTool { navigate, draw, delete }
